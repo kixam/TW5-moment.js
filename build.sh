60ef73fc108707cf951f0065b24130ab6a649e65
Aurélien Correia <aurelien.correia@gmail.com>
2017-05-18 16:04:40 +0200

    automatically update moment.js subtree

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# settings
 infofile="plugins/moment/plugin.info"
 submodulepath="plugins/moment/moment"
 
+# fetch latest version
+git submodule update --init --recursive --remote
+
+# get version number
 version="$(cd "$submodulepath" && git describe --tags $(git rev-list --tags --max-count=1))"
 expr="s/\"version\": \"[^\"]+\"/\"version\": \"$version\"/g"
 
+# update version in plugin
 sed -i -r -e "$expr" "$infofile"
+
+echo "Using moment.js version $version"
 
